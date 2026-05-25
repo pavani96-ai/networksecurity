5e56d82f28b4b99d47a49036497c8fec055f4667
pavani <pavanikusuma2020@gmail.com>
2026-05-25 07:00:35 +0000

    data transformation completed

---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,54 @@
+import os
+import pandas as pd
+import numpy as np
+import sys
+
+"""
+defining common con/stant variable for training pipeline
+"""
+TARGET_COLUMN  = "Result"
+PIPELINE_NAME: str = "NetworkSecurity"
+ARTIFACT_DIR: str = "Artifacts"
+FILE_NAME: str = "phisingData.csv"
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
+
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+
+"""
+Data Ingestion related constant start with Data_ingestion VAR NAME
+
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
+DATA_INGESTION_DATABASE_NAME: str = "PAVANIAI"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+"""
+Data Transformation related constnat start with Data_Transformation Var name
+"""
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_Data_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {"missing_values": np.nan,
+"n_neighbors": 3,"weights": "uniform",}
+
+
+
